e4ce08ce6879c9c95e50d0cc500111ff260b3c9a
Etienne Cooijmans <etienne.cooijmans@sogeti.com>
2019-06-19 01:19:26 +0200

    Redirect workaround for initializing the camera before widget.build().

--- lib/main.dart
+++ lib/main.dart
@@ -1,93 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
 
-void main() => runApp(MyApp());
+import 'widgets/camera/camerapreview.dart';
+import 'widgets/themes.dart';
 
-class MyApp extends StatelessWidget {
+void main() async {
 
-  // ---
-  // This widget is the root of your application.
-  // ---
+  List<CameraDescription> cameras;
+  CameraDescription firstCamera;
+
+  // Obtain a list of the available cameras on the device.
+  cameras = await availableCameras();
+
+  // Get a specific camera from the list of available cameras.
+  firstCamera = cameras.first;
+
+  runApp(
+    // FIXME Instead of returning a MaterialApp here,
+    // use MyApp for Hot Reload to pick up on changes.
+
+    MaterialApp(
+        title: 'Flashback',
+        debugShowCheckedModeBanner: false,
+        theme: Themes().appTheme,
+        home: CameraPreviewWidget( // fixme - redirect to FlashbackApp(working title)
+          title: 'Flashback',
+          camera: firstCamera,
+        )
+    ),
+  );
+}
+
+// fixme - lifecycle architecture issue / see hot reloading
+// todo - display list of taken pictures
+// todo - handle this as a first screen in the app
+// todo - use FutureBuilder for the async part of the sync build call!
+class FlashbackApp extends StatefulWidget {
+
+   // ---
+   // This widget is the root of your application.
+   // ---
+
+  final List<Image> _images = <Image>[];
+  int index = 0;
+
+  Widget _buildList() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(10.0),
+      itemBuilder: (context, i) {
+
+        if(_images.length == 0)
+          return Text('Your album is currently empty.', style: Theme.of(context).textTheme.display1,);
+
+        if (i.isOdd)
+          return Divider();
+
+        // todo - add description or data as a param
+        return _buildListItem(_images[index]);
+      },
+    );
+  }
+
+  // todo - add desc or data as a param
+  Widget _buildListItem(Image image) {
+    return ListTile(
+      title: Text(image.toStringShort(), style: TextTheme().display1,),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flashback',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-
-        fontFamily: 'Share Tech Mono',
-
-        // THEME COLORS:  0xFF 69858E A2BAC1 CCD7E2 EBEFF4 F4FFFB
-        // THEME ACCENTS: 0xFF 258EA6 C6878F A1D2CE
-
-        // Hexadecimals are in the 0xARGB format.
-        primaryColor: const Color(0xFF69858E),
-        secondaryHeaderColor:const Color(0xFFCCD7E2),
-        accentColor: const Color(0xFF258EA6),
-        scaffoldBackgroundColor: const Color(0xFFEBEFF4),
-
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flashback'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+      // todo - this is the before-taking-picture widget layout
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
@@ -109,20 +94,30 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+              'Your album is currently empty.',
+            style: Theme.of(context).textTheme.display1,
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      debugShowCheckedModeBanner: false,
+      theme: Themes().appTheme,
+      home: CameraPreviewWidget(
+          title: 'Flashback',
+          camera: firstCamera,
+      ),
     );
   }
+
+  @override
+  State createState() => _FlashbackAppState();
+}
+
+class _FlashbackAppState extends State<FlashbackApp> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+
 }
